ccbd7c437e3b693f8196584cde2ee7fffba83884
Fernando Baltazar <fernando.baltazarv@gmail.com>
2022-08-08 12:37:39 -0500

    For loop

---
+++ c++/introduction/data_types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int en;
+    long lon;
+    char ch;
+    float fl;
+    double dou;
+    scanf("%d %ld %c %f %lf", &en, lon, ch, fl, dou);
+    printf("%d %ld %c %f %lf", en, lon, ch, fl, dou);
+
+    return 0;
+}
