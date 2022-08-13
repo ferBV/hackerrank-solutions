15ca8e95858796223297e0c8e104a1a68642d4d7
Fernando Baltazar <fernando.baltazarv@gmail.com>
2022-08-13 15:47:49 -0500

    Finish Introduction

--- c++/introduction/VariableArrays.cpp
+++ c++/introduction/VariableArrays.cpp
@@ -7,6 +7,32 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	int n, q, k, in;
+	cin >> n >> q;
+	vector<vector<int>> a;
+	vector<int> b;
+
+	/* Entrada datos */
+	for(int i = 0; i<n; i++){
+		cin >> k;
+		for(int j = 0; j<k; j++){
+		cin >>in;
+		b.push_back(in);
+		}
+		a.push_back(b);
+		b.clear();
+	}
+	/* Entrada preguntas */
+	int m,l;
+
+	for(int i = 0; i<q; i++){
+		cin >> m >> l;
+		b.push_back(m); b.push_back(l);
+	}
+
+	for(int i =0; i<q*2; i+=2){
+		cout << a[b[i]][b[i+1]] << endl;
+	}
+
     return 0;
 }
